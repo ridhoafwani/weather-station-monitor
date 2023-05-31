034b365b6f8b2b83ae08d296c05ebbf1e9884019
ridhoafwani <zikrurridhoafwani@gmail.com>
2023-05-31 20:55:35 +0800

    fix some warnings

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,7 +7,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  
+  apiKey: "-MJ5WaY-ca5iySsR4c",
+  authDomain: "mini-wheater-station.firebaseapp.com",
+  databaseURL: "https://mini-wheater-station-default-rtdb.firebaseio.com",
+  projectId: "mini-wheater-station",
+  storageBucket: "mini-wheater-station.appspot.com",
+  messagingSenderId: "64722236595",
+  appId: "1:64722236595:web:51c0c636cb265a023becc6",
+  measurementId: "G-3YK39K7VZD"
 };
 
 // Initialize Firebase
