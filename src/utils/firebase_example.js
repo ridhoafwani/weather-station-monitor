c019509f8fb68dbfd8bfb03c66de138a1d9efb03
ridhoafwani <zikrurridhoafwani@gmail.com>
2023-06-05 13:26:43 +0000

    add firebase.js example

---
+++ src/utils/firebase_example.js
@@ -0,0 +1,26 @@
+// This is example file for firebase.js file, rename this file into firebase.js and insert your firebase config data
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
